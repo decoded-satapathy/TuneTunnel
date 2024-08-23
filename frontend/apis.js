98b1643ad7cd6de588c8e2645163c27fb5314304
Om Satapathy <omsatapathy05@gmail.com>
2024-08-24 01:59:16 +0530

    fixed fullScreenSong.jsx

--- frontend/apis.js
+++ frontend/apis.js
@@ -1,8 +1,8 @@
 export const All_API = {
-  "search": "http://localhost:3001/api/v1/search?q=",
+  search: "http://localhost:3001/api/v1/search?q=",
   //  "lyrics" : "http://localhost:3000/api/v1/lyrics",
   //  "stream" : "http://localhost:3000/api/v1/stream?videoId=fsiPzT50ZiM"
-  "lyrics": "http://localhost:3001/api/v1/lyrics",
-  "artist": "http://localhost:3001/api/v1/search/artist",
-  "songUrl" : "http://192.168.31.98:3001/api/v1/stream?videoId="
-}
+  lyrics: "http://localhost:3001/api/v1/lyrics",
+  artist: "http://localhost:3001/api/v1/search/artist/",
+  songUrl: "http://192.168.31.98:3001/api/v1/stream?videoId=",
+};
