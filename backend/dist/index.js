379b60c773fb353a43b78d14a2ff4b1ef9a3d69a
Om Satapathy <omsatapathy05@gmail.com>
2024-09-06 22:53:43 +0530

    Fixed the issue of backend folder submodule

---
+++ backend/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const signin_1 = require("./routes/signin");
+const download_1 = require("./routes/download");
+const lyrics_1 = require("./routes/lyrics");
+const search_1 = require("./routes/search");
+const signup_1 = require("./routes/signup");
+const stream_1 = __importDefault(require("./routes/stream"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/api/v1/download", download_1.downloadRouter);
+app.use("/api/v1/lyrics", lyrics_1.lyricsRouter);
+app.use("/api/v1/search", search_1.searchRouter);
+app.use("/api/v1/signin", signin_1.signInRouter);
+app.use("/api/v1/signup", signup_1.signUpRouter);
+app.use("/api/v1/stream", stream_1.default);
+app.listen(3001);
+console.log("Server is listening on port 3001");
